d24512ff420aa76af09df1184920935362f4b912
Lucian Popescu <lucian.gabriel.popescu@gmail.com>
2017-03-28 14:18:35 +0300

    Fixed bugs following tests!

--- src/SysLogger.php
+++ src/SysLogger.php
@@ -1,8 +1,9 @@
 <?php
+require_once("Logger.php");
 /**
  * Logs messages/errors into syslog service.
  */
-class SysLogger extends DiskLogger {
+class SysLogger extends Logger {
 	private $applicationName;
 	
 	/**
@@ -15,11 +16,28 @@ class SysLogger extends DiskLogger {
 	
 	/**
 	 * {@inheritDoc}
+	 * @see Logger::getErrorInfo()
+	 */
+	protected function getErrorInfo(Exception $exception) {
+		return get_class($exception)." ".$exception->getFile()." ".$exception->getLine()." ".$exception->getMessage();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see Logger::getMessageInfo()
+	 */
+	protected function getMessageInfo($message) {
+		$trace = debug_backtrace()[1];
+		return $trace["file"]." ".$trace["line"]." ".$message;
+	}
+	
+	/**
+	 * {@inheritDoc}
 	 * @see Logger::log()
 	 */
 	protected function log($message, $logLevel) {
 		openlog($this->applicationName, LOG_NDELAY, LOG_USER);
-		syslog($logLevel, $message);
+		syslog($logLevel, $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']." ".$message);
 		closelog();
 	}
 }
