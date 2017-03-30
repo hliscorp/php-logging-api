9700e24f01555249e7fa9e9e3a201efa51553fd9
Lucian Popescu <lucian.gabriel.popescu@gmail.com>
2017-03-30 14:22:20 +0300

    bugfix

--- src/SQLLogger.php
+++ src/SQLLogger.php
@@ -1,6 +1,4 @@
 <?php
-require_once("LoggingEnvironment.php");
-
 /**
  * Logs messages/errors into SQL database. Requires a table with this structure (example @ MySQL):
  * 
@@ -15,7 +13,7 @@ require_once("LoggingEnvironment.php");
  	line SMALLINT UNSIGNED NOT NULL,
  	message TEXT NOT NULL,
  	environment TEXT NOT NULL,
- 	exception TEXT NOT NULL,
+ 	trace TEXT NOT NULL,
  	date_added TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
  	primary key(id),
  	key(uid)
@@ -48,9 +46,9 @@ class SQLLogger extends Logger {
 		$this->statement = $connection->createPreparedStatement();
 		$this->statement->prepare("
 		INSERT INTO ".$tableName.($rotationPattern?"__".date($rotationPattern):"")."
-			(uid, level, url, type, file, line, message, environment, exception)
+			(uid, level, url, type, file, line, message, environment, trace)
         VALUES
-			(:uid, :level, :url, :type, :file, :line, :message, :environment, :exception)");
+			(:uid, :level, :url, :type, :file, :line, :message, :environment, :trace)");
 	}
 	
 	/**
@@ -59,14 +57,28 @@ class SQLLogger extends Logger {
 	 * @return LoggingEnvironment
 	 */
 	private function getEnvironmentInfo() {
-		$environment = new LoggingEnvironment();
-		$environment->get = (!empty($_GET)?$_GET:array());
-		$environment->post = (!empty($_POST)?$_POST:array());
-		$environment->server = (!empty($_SERVER)?$_SERVER:array());;
-		$environment->files = (!empty($_FILES)?$_FILES:array());;
-		$environment->cookies = (!empty($_COOKIE)?$_COOKIE:array());;
-		$environment->session = (!empty($_SESSION)?$_SESSION:array());;
+		$environment = array();
+		$environment["get"] = (!empty($_GET)?$_GET:array());
+		$environment["post"] = (!empty($_POST)?$_POST:array());
+		$environment["server"] = (!empty($_SERVER)?$_SERVER:array());;
+		$environment["files"] = (!empty($_FILES)?$_FILES:array());;
+		$environment["cookies"] = (!empty($_COOKIE)?$_COOKIE:array());;
+		$environment["session"] = (!empty($_SESSION)?$_SESSION:array());;
 		return $environment;
+	}
+	
+	/**
+	 * Strips trace of anything but file and line.
+	 * 
+	 * @param array $trace PHP-formatted trace
+	 * @return array Simplified trace
+	 */
+	private function stripTrace($trace) {
+		$output = array();
+		foreach($trace as $item) {
+			$output[]=array("file"=>$item["file"],"line"=>$item["line"]);
+		}
+		return $output;
 	}
 	
 	/**
@@ -82,7 +94,7 @@ class SQLLogger extends Logger {
 			":line"=>$exception->getLine(),
 			":message"=>$exception->getMessage(),
 			":environment"=>serialize($this->getEnvironmentInfo()),
-			":exception"=>serialize($exception)
+			":trace"=>serialize($this->stripTrace($exception->getTrace()))
 		);
 	}
 	
@@ -92,15 +104,17 @@ class SQLLogger extends Logger {
 	 */
 	protected function getMessageInfo($message) {
 		// return a query
-		$trace = debug_backtrace()[1];
+		$trace = debug_backtrace();
+		unset($trace[0]);
+		
 		return array(
 			":url"=>$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'],
 			":type"=>self::LOGGING_TYPE,
-			":file"=>$trace["file"],
-			":line"=>$trace["line"],
+			":file"=>$trace[1]["file"],
+			":line"=>$trace[1]["line"],
 			":message"=>$message,
 			":environment"=>serialize($this->getEnvironmentInfo()),
-			":exception"=>""
+			":trace"=>serialize($this->stripTrace($trace))
 		);
 	}
 	
